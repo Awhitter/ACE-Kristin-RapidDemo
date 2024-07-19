3d2fe150609b16aa27d9d31f7654bf5f967ed3f5
Your Name <you@example.com>
2024-07-19 07:33:17 -0300

    Integrated the HashRouter component and imported the Tailwind CSS stylesheet.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import './tailwind.css';
 import './index.css';
 import ACEInhibitorsGuide from './ACEInhibitorsGuide';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ACEInhibitorsGuide />
+    <HashRouter>
+      <ACEInhibitorsGuide />
+    </HashRouter>
   </React.StrictMode>
 );
 
